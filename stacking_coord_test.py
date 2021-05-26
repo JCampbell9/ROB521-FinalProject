5f833c481a7c904362bd9cc7081eafeec8b96b39
NatashaTroxler <47993865+NatashaTroxler@users.noreply.github.com>
2021-05-26 15:35:07 -0700

    testing grid coordinates

--- stacking_coord_test.py
+++ stacking_coord_test.py
@@ -2,8 +2,8 @@
 # coding=utf8
 
 
-import Camera_Motion
-import CameraPerception
+import Motion
+import Perception
 
 import sys
 
@@ -15,22 +15,22 @@ import Camera
 
 def test_motion(target_color='red'):
     goal_coordinates = {
-        'BL': (-14, -10.5, 1.5),
-        'BM': (-14, -7.5, 1.5),
-        'BR': (-14, -4.5, 1.5),
-        'ML': (-14, -10.5, 4.5),
-        'MM': (-14, -7.5, 4.5),
-        'MR': (-14, -4.5, 4.5),
-        'TL': (-14, -10.5, 7.5),
-        'TM': (-14, -7.5, 7.5),
-        'TR': (-14, -4.5, 7.5)
+        'BL': (-14, -7.5, 1.5),
+        'BM': (-14, -4.5, 1.5),
+        'BR': (-14, -1.5, 1.5),
+        'ML': (-14, -7.5, 4.5),
+        'MM': (-14, -4.5, 4.5),
+        'MR': (-14, -1.5, 4.5),
+        'TL': (-14, -7.5, 7.5),
+        'TM': (-14, -4.5, 7.5),
+        'TR': (-14, -1.5, 7.5)
     }
 
     my_camera = Camera.Camera()
     my_camera.camera_open()
     img = my_camera.frame
-    camera_perception = CameraPerception.Perception()
-    motion = Camera_Motion.MoveBlock()
+    camera_perception =Perception.Perception()
+    motion = Motion.MoveBlock()
 
     while True:
         img = my_camera.frame
@@ -60,18 +60,21 @@ def test_motion(target_color='red'):
             motion.close_gripper()
             # lift block up
             print('\n lift block up')
-            motion.move_arm((coordinates[0], coordinates[1], 11.5), time_delay=1)
-            goal_loc = goal_coordinates['BL']
+            motion.move_arm((coordinates[0], coordinates[1], 11.5), time_delay=1.5)
+            print('\n lift block up')
+            goal_loc = goal_coordinates['BR']
+            # goal_loc = (-15 + 1, -7 - 0.5, 1.5)
+            print(f'\n\n goal loc: {goal_loc} \n\n')
             # move block above goal location
-            motion.move_arm((goal_loc[0], goal_loc[1], goal_loc[2] + 8), time_delay=False)
+            motion.move_arm((goal_loc[0], goal_loc[1], goal_loc[2] + 8), time_delay=1.5)
             # properly angle gripper
-            motion.angle_gripper((goal_loc[0], goal_loc[1], -90))
+            motion.angle_gripper((goal_loc[0], goal_loc[1], 0))
             # lower the gripper to 3cm above the final z
-            motion.move_arm((goal_loc[0], goal_loc[1], goal_loc[2] + 3), time_delay=1)
+            motion.move_arm((goal_loc[0], goal_loc[1], goal_loc[2] + 3), time_delay=1.5)
             # set block down
-            motion.move_arm(goal_loc, time_delay=0.5)
+            motion.move_arm(goal_loc, time_delay=1.5)
             motion.open_gripper()
-            motion.move_arm((goal_loc[0], goal_loc[1], goal_loc[2] + 10), time_delay=0.8)
+            motion.move_arm((goal_loc[0], goal_loc[1], goal_loc[2] + 10), time_delay=1.5)
             motion.init_pose()
             break
 
@@ -80,7 +83,7 @@ def camera_test():
     my_camera = Camera.Camera()
     my_camera.camera_open()
     img = my_camera.frame
-    camera_perception = CameraPerception.Perception()
+    camera_perception = Perception.Perception()
     i = 0
     while True:
 
